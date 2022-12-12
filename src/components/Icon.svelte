67e3b4618eb1eb691a663797542cf32ce6d5b1f6
Armando M <cuentafrst@gmail.com>
2022-12-12 01:51:35 -0600

    remove icon svelte props

--- src/components/Icon.svelte
+++ src/components/Icon.svelte
@@ -1,15 +1,8 @@
 <script lang="ts">
-  import { cssCustomProps } from 'utils/dom';
   import iconError from 'icons/triangle-exclamation.svg?raw';
   import iconLoading from 'icons/loading.svg?raw';
 
-  type HSL = [h: number, s: number, l: number];
-
   export let name: IconName;
-  export let size: string | undefined = undefined;
-  export let primary: HSL | undefined = undefined;
-  export let secondary: HSL | undefined = undefined;
-  export let tertiary: HSL | undefined = undefined;
 
   async function loadIcon(iconName: IconName) {
     return (await import(`icons/${iconName}.svg?raw`)).default as string;
@@ -17,32 +10,14 @@
 
   $: quotedName = `"${name}"`;
   $: iconPromise = loadIcon(name);
-  $: svgColors = (
-    (primary ? cssCustomProps({
-      iconSize: size,
-      iconPrimaryH: primary[0],
-      iconPrimaryS: `${primary[1]}%`,
-      iconPrimaryL: `${primary[2]}%`,
-    }) : '') +
-    (secondary ? cssCustomProps({
-      iconSecondaryH: secondary[0],
-      iconSecondaryS: `${secondary[1]}%`,
-      iconSecondaryL: `${secondary[2]}%`,
-    }) : '') +
-    (tertiary ? cssCustomProps({
-      iconTertiaryH: tertiary[0],
-      iconTertiaryS: `${tertiary[1]}%`,
-      iconTertiaryL: `${tertiary[2]}%`,
-    }) : '')
-  );
 </script>
 
 {#await iconPromise}
-  <span class="Icon Icon--rotate {$$props.class ?? ''}" style={svgColors} title="Loading {quotedName} icon...">
+  <span class="Icon Icon--rotate {$$props.class ?? ''}" title="Loading {quotedName} icon...">
     {@html iconLoading}
   </span>
 {:then icon}
-  <span class="Icon {$$props.class ?? ''}" style={svgColors}>
+  <span class="Icon {$$props.class ?? ''}">
     {@html icon}
   </span>
 {:catch}
